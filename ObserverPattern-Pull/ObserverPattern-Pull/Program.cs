e73291749877f5986cc92425413f9b32a5769cc5
dimitarvikentiev <dimitar.vikentiev@gmail.com>
2016-03-01 01:22:12 +0100

    UI update

--- ObserverPattern-Pull/ObserverPattern-Pull/Program.cs
+++ ObserverPattern-Pull/ObserverPattern-Pull/Program.cs
@@ -16,7 +16,7 @@ namespace ObserverPattern_Pull
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new ObserverPatternForm());
         }
     }
 }
