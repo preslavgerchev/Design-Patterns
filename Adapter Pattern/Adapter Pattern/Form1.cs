0176b4488711d2464763b8a3ead8313993ded7b1
dimitarvikentiev <dimitar.vikentiev@gmail.com>
2016-03-21 10:20:42 +0100

    UI

--- Adapter Pattern/Adapter Pattern/Form1.cs
+++ Adapter Pattern/Adapter Pattern/Form1.cs
@@ -15,8 +15,45 @@ namespace Adapter_Pattern
         public Form1()
         {
             InitializeComponent();
-            IDigitalSignalTransmitter digitalTransmitter = new AnalogToDigitalAdapter(new AnalogTransmitter());
-            label1.Text = digitalTransmitter.TransmitDigitalData("Beep beep");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != string.Empty)
+            {
+                IAnalogSignalTransmitter AS = new AnalogTransmitter();
+                label1.Text = AS.TransmitAnalogData(textBox1.Text);
+            }
+            else
+            {
+                label1.Text = "You need to write some information.";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != string.Empty)
+            {
+                IDigitalSignalTransmitter DS = new DigitalTransmitter();
+                label1.Text = DS.TransmitDigitalData(textBox1.Text);
+            }
+            else
+            {
+                label1.Text = "You need to write some information.";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != string.Empty)
+            {
+                IDigitalSignalTransmitter digitalTransmitter = new AnalogToDigitalAdapter(new AnalogTransmitter());
+                label1.Text = digitalTransmitter.TransmitDigitalData(textBox1.Text);
+            }
+            else
+            {
+                label1.Text = "You need to write some information.";
+            }
         }
     }
 }
