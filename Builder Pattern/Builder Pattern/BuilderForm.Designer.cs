ad68802e4bb0bda2fdae976182014d4c963c127f
dimitarvikentiev <dimitar.vikentiev@gmail.com>
2016-03-29 09:19:02 +0200

    UI update

--- Builder Pattern/Builder Pattern/BuilderForm.Designer.cs
+++ Builder Pattern/Builder Pattern/BuilderForm.Designer.cs
@@ -29,24 +29,62 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 70);
+            this.label1.Location = new System.Drawing.Point(253, 44);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(46, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             // 
-            // Form1
+            // button1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.button1.Location = new System.Drawing.Point(25, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "BMW";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(25, 98);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 53);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Audi";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(25, 172);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 53);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Mercedes";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // BuilderForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(491, 253);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
-            this.Name = "Form1";
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "BuilderForm";
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -56,6 +94,9 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Builder Pattern/Builder Pattern/BuilderForm.cs
+++ Builder Pattern/Builder Pattern/BuilderForm.cs
@@ -12,13 +12,36 @@ namespace Builder_Pattern
 {
     public partial class BuilderForm : Form
     {
+        private ICarBuilder bmwBuilder;
+        private ICarBuilder audiBuilder;
+        private ICarBuilder mercedesBuilder;
+        private CarConstructor carConstructor;
+
         public BuilderForm()
         {
             InitializeComponent();
-            ICarBuilder bmwBuilder = new BMWBuilder();
-            CarConstructor carConstructor = new CarConstructor();
+            carConstructor = new CarConstructor();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bmwBuilder = new BMWBuilder();
             carConstructor.Construct(bmwBuilder);
             label1.Text = bmwBuilder.Car.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            audiBuilder = new AudiBuilder();
+            carConstructor.Construct(audiBuilder);
+            label1.Text = audiBuilder.Car.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            mercedesBuilder = new MercedesBuilder();
+            carConstructor.Construct(mercedesBuilder);
+            label1.Text = mercedesBuilder.Car.ToString();
         }
     }
 }
